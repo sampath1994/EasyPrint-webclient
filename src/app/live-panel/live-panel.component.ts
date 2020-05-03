1a29bd762a1f6d6e2f1043fecd0e3af850058d07
promod94 <webpromod@gmail.com>
2020-05-03 13:50:58 +0530

    Map poll info to ui

--- src/app/live-panel/live-panel.component.ts
+++ src/app/live-panel/live-panel.component.ts
@@ -14,11 +14,14 @@ import { take } from 'rxjs/operators';
 export class LivePanelComponent implements OnInit {
 
   private bodyText: string;
-  printjobs: PrintJob[];
+  printjobs: PrintJob[] = [];
   currentPrintJob: PrintJob;
+  private totalElements: number;
+  private totalPages: number;
+  private currentPage: number;
 
   constructor(private modalService: ModalService, private poll: PollService, private messagingService: MessagingService ) { 
-    this.printjobs = [{"id":1,"username":"sam","docname":"testdoc","color":"black"},{"id":2,"username":"pro","docname":"testdoc2","color":"clr"}]
+    //this.printjobs = [{"id":1,"username":"sam","docname":"testdoc","color":"black"},{"id":2,"username":"pro","docname":"testdoc2","color":"clr"}]
 
   }
 
@@ -27,7 +30,7 @@ export class LivePanelComponent implements OnInit {
     let pollSubscription$ =  this.poll.getPrintJobPoll();
     let pollError$ = this.poll.getErrorState();
     pollError$.subscribe(data => console.log('Error state', data));
-    let pollsubs = pollSubscription$.subscribe(data => {console.log(data/*.content*/); pollError$.next(false);},error => console.log('Error'));
+    let pollsubs = pollSubscription$.subscribe(data => {this.currentLivePanelDataExtract(data); pollError$.next(false);},error => console.log('Error'));
     let notiPermission$ = this.messagingService.getNotiPermission();
     notiPermission$.pipe(take(1)).subscribe(data => {
       if(data){
@@ -47,4 +50,17 @@ closeModal(id: string) {
     this.modalService.close(id);
 }
 
+currentLivePanelDataExtract(pageInfo: any){
+  console.log(pageInfo);
+  let pageArray = pageInfo.content;
+  this.totalElements = pageInfo.totalElements;
+  this.totalPages = pageInfo.totalPages;
+  this.currentPage = pageInfo.number;
+  this.printjobs = [];
+  for(let idx in pageArray){
+    let printj: PrintJob = <PrintJob> pageArray[idx];
+    console.log(printj);
+    this.printjobs.push(printj);  
+  }
+}
 }
--- src/app/models/print-job.ts
+++ src/app/models/print-job.ts
@@ -1,13 +1,22 @@
-export class PrintJob {
-    id: number;
+export interface PrintJob {
+    /*id: number;
     username: String;
     docname: String;
-    color:String;
+    color:String;*/
 
-    constructor(id: number, username: String, docname: String, color:String){
+    client_id: number;
+    color: string;
+    document_id: number;
+    download_link: string;
+    ref_no: string;
+    side: string;
+    status: string;
+
+
+    /*constructor(id: number, username: String, docname: String, color:String){
         this.id = id;
         this.username = username;
         this.docname = docname;
         this.color = color;
-    }
+    }*/
 }
